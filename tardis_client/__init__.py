e23048dd31cee7d398e15b3c6ce22cc6af179c10
Thaddeus <thad@tardis.dev>
2020-04-14 12:46:38 +0200

    chore: remove old client

--- tardis_client/__init__.py
+++
@@ -1,4 +0,0 @@
-__version__ = "1.2.4"
-from tardis_client.tardis_client import TardisClient
-from tardis_client.channel import Channel
-from tardis_client.reconstructors.market_reconstructor import MESSAGE_TYPE, BOOK_UPDATE_TYPE
