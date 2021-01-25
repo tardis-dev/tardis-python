fbd9599c40186a4c19b38ba624fde4e9c41d9682
Thaddeus <thad@tardis.dev>
2021-01-25 12:07:08 +0100

    bump aiohttp version to 3.7.3

--- tardis_client/__init__.py
+++ tardis_client/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.2.8"
+__version__ = "1.2.9"
 from tardis_client.tardis_client import TardisClient
 from tardis_client.channel import Channel
 from tardis_client.reconstructors.market_reconstructor import MESSAGE_TYPE, BOOK_UPDATE_TYPE
