83bb4d2470d7866742c65f61d5623ff67f101b8f
Thaddeus <thad@tardis.dev>
2022-06-03 11:39:52 +0200

    chore: bump version

--- tardis_client/__init__.py
+++ tardis_client/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.3.1"
+__version__ = "1.3.2"
 from tardis_client.tardis_client import TardisClient
 from tardis_client.channel import Channel
 from tardis_client.reconstructors.market_reconstructor import MESSAGE_TYPE, BOOK_UPDATE_TYPE
